b831b692094966f66799233eb371e8d4d54a03f2
hpcsii <13071298568@163.com>
2019-04-04 14:26:20 +0800

    DaKa

---
+++ bop-parent/bop-common/src/main/java/com/csii/Timer/HttpUtil.java
@@ -0,0 +1,13 @@
+/**
+ * @description:
+ * @author: hp
+ * @create: 2019-04-03 10:52
+ **/
+package com.csii.Timer;
+
+public class HttpUtil {
+    public static void main(String[] args) {
+
+    }
+
+}
